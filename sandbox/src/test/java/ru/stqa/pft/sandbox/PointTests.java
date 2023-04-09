e1e47fb0e29dbbcdbe583c3e6220b331fea076b3
Shukay <shukaev.igor@bk.ru>
2023-04-09 13:17:50 +0300

    task#3 (add PointTests)

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.text.DecimalFormat;
+
+public class PointTests {
+
+    DecimalFormat decimalFormat = new DecimalFormat( "#.###" );
+
+    @Test
+    public void testDistance1() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(Point.distance(p1, p2), 0);
+    }
+
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(-1,-1);
+        Point p2 = new Point(1,1);
+        Assert.assertEquals(decimalFormat.format(Point.distance(p1, p2)), "2,828");
+    }
+
+    @Test
+    public void testDistance3() {
+        Point p1 = new Point(10,10);
+        Point p2 = new Point(15,15);
+        Assert.assertEquals(decimalFormat.format(Point.distance(p1, p2)), "7,071");
+    }
+}
