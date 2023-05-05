48c3a72130537e9732948129e488fe227678a8ec
Shukay <shukaev.igor@bk.ru>
2023-05-05 16:53:04 +0300

    fluent_interface

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -14,7 +14,7 @@ public class GroupDeletionTests extends TestBase{
     public void ensurePreconditions() {
         app.goTo().GroupPage();
         if (app.group().list().size() == 0) {
-            app.group().create(new GroupData("test123", null, null));
+            app.group().create(new GroupData().withName("test123"));
         }
     }
 
