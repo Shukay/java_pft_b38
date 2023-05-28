4833b7061c99f7222ac64331caa9bcfcceb65712
Shukay <shukaev.igor@bk.ru>
2023-05-29 00:58:46 +0300

    http, ftp, mail

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -17,8 +17,13 @@ import java.util.Properties;
 
 public class ApplicationManager {
     private final Properties properties;
+    private WebDriver wd;
+
     private String browser;
-    protected WebDriver wd;
+    private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+    private MailHelper mailHelper;
+    private JamesHelper jamesHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -28,28 +33,66 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (browser.equals(Browser.CHROME.browserName())) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            wd = new ChromeDriver(options);
-        } else if (browser.equals(Browser.EDGE.browserName())){
-            EdgeOptions options = new EdgeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            wd = new EdgeDriver(options);
-        } else if (browser.equals(Browser.FIREFOX.browserName())){
-            wd = new FirefoxDriver();
-        }
-
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        wd.get(properties.getProperty("web.baseUrl"));
-
     }
 
 
     public void stop() {
-        wd.findElement(By.linkText("Logout")).click();
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(Browser.CHROME.browserName())) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                wd = new ChromeDriver(options);
+            } else if (browser.equals(Browser.EDGE.browserName())){
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                wd = new EdgeDriver(options);
+            } else if (browser.equals(Browser.FIREFOX.browserName())){
+                wd = new FirefoxDriver();
+            }
+        }
+        return wd;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
+    }
+
+    public JamesHelper james() {
+        if (jamesHelper == null) {
+            jamesHelper = new JamesHelper(this);
+        }
+        return jamesHelper;
     }
 
 }
